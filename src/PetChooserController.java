c2bc94623796a3f1adac8ad1581d2e21dfca6dc9
giorgizena123 <zenaishvili621@gmail.com>
2025-04-16 00:52:19 +0400

    Update PetChooserController.java

--- src/PetChooserController.java
+++ src/PetChooserController.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class PetChooserController {
+
+    @FXML
+    private RadioButton catRadioButton;
+
+    @FXML
+    private RadioButton dogRadioButton;
+
+    @FXML
+    private RadioButton rabbitRadioButton;
+
+    @FXML
+    private RadioButton birdRadioButton;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private Button chooseButton;
+
+    @FXML
+    private Label resultLabel;
+
+    @FXML
+    private ToggleGroup petGroup;
+
+    @FXML
+    private void choosePet() {
+        RadioButton selectedRadioButton = (RadioButton) petGroup.getSelectedToggle();
+        if (selectedRadioButton != null) {
+            String selectedPet = selectedRadioButton.getText();
+            String petName = nameTextField.getText();
+            if (!petName.isEmpty()) {
+                resultLabel.setText("შენ აირჩიე " + selectedPet + " სახელად " + petName);
+            } else {
+                resultLabel.setText("შეიყვანეთ თქვენი შინაური ცხოველის სახელი.");
+            }
+        } else {
+            resultLabel.setText("აირჩიეთ შინაური ცხოველი.");
+        }
+    }
 }
